c7d5a6e5e7a880419a21dff8fdb7b6d2298a8979
Neel Sharma <nsdj.sharma@gmail.com>
2022-12-04 14:51:44 +1100

    dont go overkill with memory day 1

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -4,9 +4,14 @@ pub struct DayOne;
 
 impl Day for DayOne {
     fn part_one(&self, input: &str) {
-        let result: u32 = input
+        let result: usize = input
             .split("\n\n")
-            .map(|chunk| chunk.lines().map(|line| line.parse::<u32>().unwrap()).sum())
+            .map(|chunk| {
+                chunk
+                    .lines()
+                    .map(|line| line.parse::<usize>().unwrap())
+                    .sum()
+            })
             .max()
             .unwrap();
 
@@ -16,13 +21,18 @@ impl Day for DayOne {
     fn part_two(&self, input: &str) {
         let mut cals = input
             .split("\n\n")
-            .map(|chunk| chunk.lines().map(|line| line.parse::<u32>().unwrap()).sum())
+            .map(|chunk| {
+                chunk
+                    .lines()
+                    .map(|line| line.parse::<usize>().unwrap())
+                    .sum()
+            })
             .collect::<Vec<_>>();
 
         cals.sort();
         cals.reverse();
 
-        let result: u32 = cals.iter().take(3).sum();
+        let result: usize = cals.iter().take(3).sum();
 
         println!("{}", result);
     }
